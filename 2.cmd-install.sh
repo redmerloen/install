4493ce8353f46e76121226bc12439814fe362dd5
Redmer Loen <redmerloen@gmail.com>
2019-04-02 14:00:12 +0200

    Settings

--- 2.cmd-install.sh
+++ 2.cmd-install.sh
@@ -13,11 +13,13 @@ brew cask install iterm2
 
 # download ZSH theme
 cd ~/.oh-my-zsh/themes
-curl -O "https://github.com/redmerloen/install/raw/master/cobalt2.zsh-theme"
+curl -O "https://raw.githubusercontent.com/redmerloen/install/master/.oh-my-zsh/themes/cobalt2.zsh-theme"
 
 # download zsh configration
 cd ~/.
-curl -O "https://github.com/redmerloen/install/raw/master/.zshrc"
+curl -O "https://raw.githubusercontent.com/redmerloen/install/master/.zshrc"
+curl -O "https://raw.githubusercontent.com/redmerloen/install/master/.aliasses"
+curl -O "https://raw.githubusercontent.com/redmerloen/install/master/.path_exports"
 
 # install shell integration & utilities
 curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
@@ -27,5 +29,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
+
+brew install rbenv
+rbenv init
 
 cd "$CURRENT_DIR"
